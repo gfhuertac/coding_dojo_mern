3666260017dfc19c831d39741b3eb474820f8e82
Gonzalo Huerta-Canepa <gonzalo@huerta.cl>
2020-02-20 11:09:18 -0300

    React: putting it together

--- react/first-react-w-jsx/src/App.js
+++ react/first-react-w-jsx/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import HelloWorld from './components/HelloWorld';
 import PersonCard from './components/PersonCard';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
--- react/first-react-w-jsx/src/components/PersonCard.js
+++ react/first-react-w-jsx/src/components/PersonCard.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>Birthday button for {props.name}</button>
+    );
+}
+
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {...this.props};
+    }
+
+    handleClick = () => {
+        this.setState({
+            age: this.state.age + 1
+        })
+    }
+
     render() {
-        let {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
-                <h1>{ lastName }, { firstName }</h1>
+                <h1>{ this.state.lastName }, { this.state.firstName }</h1>
                 <div>
-                    Age: { age }
+                    Age: { this.state.age }
                 </div>
                 <div>
-                    Hair color: { hairColor }
+                    Hair color: { this.state.hairColor }
                 </div>
+                <Button handleClick={this.handleClick} name={ this.state.firstName + ' ' + this.state.lastName }/>
             </div>
         );
     }
