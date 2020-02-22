f6ba75ddaa4dd5c9463082122a5c61a52321e78a
Gonzalo Huerta-Canepa <gonzalo@huerta.cl>
2020-02-22 18:54:57 -0300

    React: Big Inversion with hooks

--- react/big-inversion/src/components/PersonCard.js
+++ react/big-inversion/src/components/PersonCard.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>Birthday button for {props.name}</button>
+    );
+}
 
 const PersonCard = (props) => {
+
+    const [ageState, setAgeState] = useState({
+        age: props.age
+    });
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setAgeState({
+            age: ageState.age + 1
+        })
+    }
+
     return (
         <div>
             <h1>{ props.lastName }, { props.firstName }</h1>
             <div>
-                Age: { props.age }
+                Age: { ageState.age }
             </div>
             <div>
                 Hair color: { props.hairColor }
             </div>
+            <Button handleClick={handleClick} name={ props.firstName + ' ' + props.lastName }/>
         </div>
     );
 }
